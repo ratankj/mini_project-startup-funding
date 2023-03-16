7ac02332edb9f2313e869ce79b0d0debe6674030
ratan kumar jha <imratankj@gamil.com>
2023-03-16 17:00:56 +0530

    complted

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ df= pd.read_csv('startup_cleaned.csv')
 df['date']=pd.to_datetime(df['date'],errors='coerce')
 df['month']=df['date'].dt.month
 df['year']=df['date'].dt.year
+df['city']=df['city'].str.replace('Bangalore','Bengaluru')
 
 
 # ************************  working on overall analysis    *******************************************************************
@@ -49,6 +50,8 @@ def load_overall_analysis():
     with col4:
         st.metric('Funded startup',num_startups)
 
+
+    
     # month on month graph
 
     st.header('Month on Month graph')
@@ -67,18 +70,110 @@ def load_overall_analysis():
     tempp_df['x-axis']=tempp_df['month'].astype('str') + '-' + tempp_df['year'].astype('str')
 
     fig5,ax5=plt.subplots()
+    #plt.figure().set_figwidth(15)
     ax5.plot(tempp_df['x-axis'],tempp_df['amount'])
+   
+    ax5.set_xticklabels(tempp_df['x-axis'], fontsize=5)
+    plt.xticks(rotation='vertical')
     st.pyplot(fig5)
 
-    # 
+    # sector analysis
 
+    st.header('Top sector analysis')
+    option=st.selectbox('Select Type',['sum','Count'])
+    
+    if option =='sum':
+
+        sector_analysis= df.groupby('vertical')['amount'].sum().sort_values(ascending=False).head(10)
+
+    else:
+
+        sector_analysis= df.groupby('vertical')['amount'].count().sort_values(ascending=False).head(10)
+    
+
+    fig6,ax6=plt.subplots()
+    ax6.pie(sector_analysis,labels=sector_analysis.index,autopct="%0.01f%%")
+    st.pyplot(fig6)
+
+
+    col1,col2=st.columns(2)
+
+    with col1:
+        st.header('Type of Funding')
+
+        type_of_funding =pd.DataFrame(df['round'].unique(),columns =['Funding type'])
+
+        st.dataframe(type_of_funding)
+
+    with col2:
+        st.header('Top 10 City wise funding')
+
+        
+        city_funding =df.groupby('city')['amount'].sum().sort_values(ascending=False).head(10)
+
+        fig8,ax8=plt.subplots()
+       
+        ax8.bar(city_funding.index,city_funding.values)
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig8)
+
+    # top investor
+
+    st.header('Top Investor')
+    top_investor=df.groupby('investor')['amount'].max().sort_values(ascending=False).head(1)
+    st.dataframe(top_investor)
+
+   
 
 # ********************************** STARTUP *********************************************************************************
 
+def load_startp_details(startup):
+    st.title(startup)
 
+    col1,col2,col3=st.columns(3)
 
+    with col1:
+    # realted to which indusrty
+    
+        industry=df[df['startup'].str.contains(startup)]['vertical']
 
+        st.header('Related Industry')
+        st.dataframe(industry)
 
+    with col2:
+    # related to which sub industry
+        sub_industry=df[df['startup'].str.contains(startup)]['subvertical']
+
+        st.header('Sub Industry')
+        st.dataframe(sub_industry)
+
+    with col3:
+        location=df[df['startup'].str.contains(startup)]['city'].unique()
+
+        st.header('Location')
+        st.dataframe(location)
+
+    
+    col1,col2,col3=st.columns(3)
+    with col1:
+        stage=df[df['startup'].str.contains(startup)]['round']
+        st.header('Investment round')
+        st.dataframe(stage)
+
+    with col2:
+        investment=df[df['startup'].str.contains(startup)]['investor']
+        st.header('Investor')
+        st.dataframe(investment)
+
+    with col3:
+        date=df[df['startup'].str.contains(startup)]['date']
+        st.header('Investment date')
+        st.dataframe(date)
+
+    
+    amount=pd.DataFrame(df[df['startup'].str.contains(startup)]['amount']).sum()
+    st.header('Invested Amount')
+    st.dataframe(amount)
 
 # ************************  working on investor dropdown   *******************************************************************
 
@@ -168,9 +263,12 @@ if option == 'overall analysis':
 
 elif option=='startup':
 
-    st.sidebar.selectbox('select startup',sorted(df['Startup'].unique().tolist()))
+    select_Startup = st.sidebar.selectbox('select startup',sorted(df['startup'].unique().tolist()))
     btn1=st.sidebar.button('find startup detail')
-    st.title('startup analysis')
+    #st.title('startup analysis')
+
+    if btn1:
+        load_startp_details(select_Startup)
 
 else:
 
